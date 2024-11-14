24500addd2b83673e03ea10223d43a0b4b3b77be
Natasha Buckham <natasha.buckham@softwire.com>
2024-11-14 17:35:10 +0000

    Refactor to simplify array transformation

--- controllers/roverController.ts
+++ controllers/roverController.ts
@@ -6,13 +6,12 @@ export const RoverController = {
     Index: async (req: Request, res: Response): Promise<any> => {
         try {
             const roverData: Rover[] | string = await nasaClient.getRoverList()
-            const roverNames: string[] = []
 
             if (typeof roverData === 'string') {
                 return roverData
             }
 
-            roverData.forEach((rover: Rover) => roverNames.push(rover.name))
+            const roverNames = roverData.map((rover: Rover) => rover.name)
 
             res.send(roverNames)
         } catch(error) {
